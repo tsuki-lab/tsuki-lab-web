ab2a7d3f615be08d4c89096aa97b15e9aced5ae2
tsuki-lab <48175599+tsuki-lab@users.noreply.github.com>
2021-07-20 15:56:06 +0900

    add: notFound pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const NotFoundPage = () => {
+
+  return (
+    <main>
+      <Helmet>
+        <title>tsuki lab</title>
+      </Helmet>
+
+      <h1>Not Found</h1>
+    </main>
+  );
+}
+
+export default NotFoundPage;
