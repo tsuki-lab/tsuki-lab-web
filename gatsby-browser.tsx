b37bafbe6ef088a9e7b6c8a35a6bd7d6b0a874da
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-07-20 17:42:09 +0900

    fix: css load order bug

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,13 @@
 import React, { ReactNode } from 'react';
-import './src/styles/global.css'
+import { Helmet } from 'react-helmet';
+import './src/styles/global.scss'
 
 export const wrapPageElement = ({ element }): ReactNode => {
   return (
     <>
+      <Helmet>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css" />
+      </Helmet>
       {element}
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import '../styles/index.scss';
 import authorIcon from '../images/icon.png';
 import { useForm, ValidationError } from '@formspree/react';
 
