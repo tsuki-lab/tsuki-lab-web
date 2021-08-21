bc0d52914d59926bebe3183222c4f54be2497cc7
tsuki-lab <48175599+tsuki-lab@users.noreply.github.com>
2021-08-21 22:25:52 +0900

    rename dir

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 require("./src/styles/global.css");
 const React = require("react")
-const { Layout } = require("./src/components/organisms/_layout")
+const { Layout } = require("./src/components/organisms/Layout")
 
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
