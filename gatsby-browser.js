85c7a5bd01f9ae710ccd64bef868effedbb8fc40
tsuki-lab <48175599+tsuki-lab@users.noreply.github.com>
2021-08-21 22:32:56 +0900

    updated gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-require("./src/styles/global.css");
-const React = require("react")
-const { Layout } = require("./src/components/organisms/Layout")
+import './src/styles/global.css'
+import React from 'react'
+import { Layout } from './src/components/organisms/Layout'
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
